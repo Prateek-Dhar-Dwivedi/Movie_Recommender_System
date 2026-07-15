600354b7a5efc15b87ffbf4dc3322b8eb6dbb2f3
Prateek-Dhar-Dwivedi <prateekdhardwivedi10@gmail.com>
2026-07-15 20:24:38 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,47 @@
 const express = require("express");
 const cors = require("cors");
-
-const recommendRoute = require("./routes/recommend");
+const axios = require("axios");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/recommend", recommendRoute);
 
-app.listen(5001, () => {
-  console.log("Server running on port 5001");
+// Test route
+app.get("/", (req, res) => {
+    res.send("Movie Backend Running");
+});
+
+
+// Recommendation API
+app.get("/api/recommend/:movie", async (req, res) => {
+
+    try {
+
+        const movie = req.params.movie;
+
+        const response = await axios.get(
+            `https://movie-recommender-system-2-cvnf.onrender.com/recommend/${movie}`
+        );
+
+        res.json(response.data);
+
+    } catch (error) {
+
+        console.log(error.message);
+
+        res.status(500).json({
+            error: "Recommendation failed"
+        });
+
+    }
+
+});
+
+
+const PORT = 5001 || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
